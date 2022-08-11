bb689eca08e033c23d877bbc061de09a9569640b
Metal-Scape <93167262+Metal-Scape@users.noreply.github.com>
2022-08-11 19:05:44 +0100

    Add files via upload

---
+++ 2015 Solutions/Day 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+int floor = 0;
+int pos = 0;
+List<int> list = new List<int>();
+
+using (StreamReader file = new StreamReader("input.txt"))
+{
+        foreach (var l in file.ReadLine())
+    {
+        switch (l)
+        {
+        case '(':
+            floor += 1;
+            break;
+        case ')':
+            floor -= 1;
+            break;
+        }
+        pos++;
+        if (floor == -1)
+        {
+            list.Add(pos);
+        }
+    }
+}
+
+Console.WriteLine("Part 1: {0}\nPart 2: {1}", floor, list[0]);
